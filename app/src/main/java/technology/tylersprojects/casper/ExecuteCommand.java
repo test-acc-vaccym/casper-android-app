80abb99346f9f8bfb46539b45c8bf8e031e85b0d
Tyler Lambert <lambertyler@gmail.com>
2017-11-28 00:38:56 -0500

    add Firebase base classes and impl getContacts

---
+++ app/src/main/java/technology/tylersprojects/casper/ExecuteCommand.java
@@ -0,0 +1,154 @@
+package technology.tylersprojects.casper;
+
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import eu.chainfire.libsuperuser.Shell;
+
+/**
+ * Created by Tyler Lambert on 11/27/2017.
+ */
+
+public class ExecuteCommand extends IntentService {
+    private static final String TAG = "ExecuteCommand";
+    private static final String CMD = "Command";
+    private Command mCmd;
+
+    public enum Command {
+        WIPE,
+        NUKE,
+        GET_CONTACTS
+    }
+
+    public static void launchService(Context context, Command cmd) {
+        Log.i(TAG, "launchService");
+        if(context == null) {
+            return;
+        }
+        if(cmd == null) {
+            Log.e(TAG, "command cannot be null");
+            return;
+        }
+        if(!Shell.SU.available()) {
+            Log.e(TAG, "SU access is not available");
+            return;
+        }
+        Intent intent = new Intent(context, technology.tylersprojects.casper.ExecuteCommand.class);
+        intent.putExtra(CMD, cmd);
+        context.startService(intent);
+    }
+
+    public ExecuteCommand() {
+        super("Casper Execute Command");
+        Log.i(TAG, "default ExecuteCommand constructor");
+    }
+
+    public ExecuteCommand(String name) {
+        super(name);
+        Log.i(TAG, "ExecuteCommand constructor");
+    }
+
+    @Override
+    public int onStartCommand(@Nullable Intent intent, int flags, int startId) {
+
+        Log.i(TAG, "onStartCommand");
+        //mCmd = (Command) intent.getSerializableExtra(CMD);
+        Bundle bundle = intent.getExtras();
+        if(bundle == null) {
+            Log.e(TAG, "onStartCommand had an intent without a Bundle");
+        }
+        if(!bundle.containsKey(CMD)) {
+            Log.e(TAG, "onStartCommand had a Bundle without a command");
+        }
+        mCmd = (Command) bundle.getSerializable(CMD);
+        Log.i(TAG, "received command: "+mCmd);
+        if(mCmd == null) {
+            Log.e(TAG, "mCmd is initially null");
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        while(mCmd == null) {
+            Log.e(TAG, "mCmd is null");
+            return;
+        }
+        switch (mCmd) {
+            case WIPE:
+                wipePhone();
+                break;
+            case NUKE:
+                nukePhone();
+                break;
+            case GET_CONTACTS:
+                getContacts();
+                break;
+            default:
+                Log.e(TAG, "command not found");
+        }
+    }
+
+    /**
+     * Overwrites the data and cache partitions to remove user data.
+     * @return
+     */
+    public boolean wipePhone() {
+        Log.i(TAG, "inside of wipePhone function");
+
+        return false;
+    }
+
+    /**
+     * Overwrites the data and cache partitions to remove user data and then overwrites the
+     * bootloader and EFS partition. This will render the phone useless... be careful.
+     * @return
+     */
+    public boolean nukePhone() {
+
+        return false;
+    }
+
+    /**
+     * Sends the contact SQLite database to the admin where it is then parsed and presented in
+     * a readable format.
+     * @return
+     */
+    public boolean getContacts() {
+        Log.i(TAG, "inside getContacts");
+        if(!isToolboxAvailable()) {
+            Log.e(TAG, "getContacts could not find Toolbox");
+            return false;
+        }
+        Shell.SU.run(new String[]{
+                "cd /data/local/tmp",
+                "mkdir contacts",
+                "cp /data/data/*.providers.contacts/databases/* ./contacts",
+                "tar -cf contacts.tar ./contacts/*",
+                "chmod 777 contacts.tar",
+                "rm -rf ./contacts"
+        });
+        // TODO: send contacts.tar to remote server
+        Log.i(TAG, "getContacts complete");
+        return false;
+    }
+
+    /**
+     * Determines is our shell can access Toolbox.
+     * @return
+     */
+    private boolean isToolboxAvailable() {
+        Log.i(TAG, "inside isToolboxAvail");
+        try {
+            return Shell.SU.run("toolbox").get(0).equals("Toolbox!");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
--- app/src/main/java/technology/tylersprojects/casper/MainActivity.java
+++ app/src/main/java/technology/tylersprojects/casper/MainActivity.java
@@ -2,12 +2,49 @@ package technology.tylersprojects.casper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+import eu.chainfire.libsuperuser.Shell;
 
 public class MainActivity extends AppCompatActivity {
+    public final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void listData(View v) {
+        ExecuteCommand.launchService(this, ExecuteCommand.Command.GET_CONTACTS);
+        /*
+        // Note if you call SU then SH the SH will prob run with SU priv so always run it in new intent
+        List<String> resp = Shell.SU.run("lss");
+        // on fail the resp size is zero!!
+        if(resp.size() == 0) {
+            Log.i(TAG, "its zero");
+            Shell.SU.clearCachedResults();
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<resp.size(); i++) {
+            sb.append(resp.get(i)).append("\n");
+        }
+        ((TextView) findViewById(R.id.output)).setText(sb.toString());
+        resp = Shell.SU.run("ls /data/data/com.sec.knox.switcher/");
+        if(resp.size() == 0) {
+            Log.i(TAG, "its zero");
+        }
+        for(int i=0; i<resp.size(); i++) {
+            sb.append(resp.get(i)).append("\n");
+        }
+        Log.i(TAG, sb.toString());
+        */
     }
 }
