a7261b31dcc05689372f0f1b6e51cbf3d8251047
Tyler Lambert <lambertyler@gmail.com>
2017-11-30 05:04:15 -0500

    receives notifications from web console

--- app/src/main/java/technology/tylersprojects/casper/MainActivity.java
+++ app/src/main/java/technology/tylersprojects/casper/MainActivity.java
@@ -1,17 +1,8 @@
 package technology.tylersprojects.casper;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
-
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.util.List;
-
-import eu.chainfire.libsuperuser.Shell;
 
 public class MainActivity extends AppCompatActivity {
     public final String TAG = "MainActivity";
@@ -20,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void listData(View v) {
