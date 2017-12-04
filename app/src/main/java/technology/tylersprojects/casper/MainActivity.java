5ed3b0ad89d2ea1d4f0b399932c40a3b3908d4a5
Tyler Lambert <lambertyler@gmail.com>
2017-12-04 04:52:30 -0500

    working start on boot and add app icon

--- app/src/main/java/technology/tylersprojects/casper/MainActivity.java
+++ app/src/main/java/technology/tylersprojects/casper/MainActivity.java
@@ -1,20 +1,20 @@
 package technology.tylersprojects.casper;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
-    public final String TAG = "MainActivity";
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        finish();
     }
 
-    public void listData(View v) {
-        ExecuteCommand.launchService(this, ExecuteCommand.Command.WIPE);
+    @Override
+    protected void onDestroy() {
+        Log.i("Casper", "app is getting destroyed");
+        super.onDestroy();
     }
 }
---
+++ app/src/main/java/technology/tylersprojects/casper/MainService.java
@@ -0,0 +1,37 @@
+package technology.tylersprojects.casper;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MainService extends Service {
+    private static final String TAG = "MainService";
+
+
+    public MainService() {
+        int i = 0;
+        while(i < 100) {
+            i++;
+            try {
+                Thread.sleep(5000);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+            Log.d(TAG, "Casper still going");
+        }
+    }
+
+    // Make sure this service is not killed.
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG, "Casper MainService started");
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
